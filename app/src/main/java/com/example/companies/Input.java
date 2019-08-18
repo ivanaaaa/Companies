434870eeeff718565177624a3b44dc681042a355
ivanaaaa <35149883+ivanaaaa@users.noreply.github.com>
2019-08-18 14:58:11 +0200

    onCreate and onClick separated

--- app/src/main/java/com/example/companies/Input.java
+++ app/src/main/java/com/example/companies/Input.java
@@ -13,7 +13,9 @@ import android.widget.Checkable;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.firebase.FirebaseApp;
 import com.firebase.client.Firebase;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,21 +23,23 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
 import java.text.DecimalFormat;
 
 import static android.text.TextUtils.*;
 
 public class Input extends AppCompatActivity {
-   public ImageView input;
+    public ImageView input;
+    Toolbar appbarid;
     public EditText name_company;
     public EditText address_company;
     public EditText latitude;
     public EditText longitude;
-    public EditText email,phone,web;
-    private CheckBox services_check, fun_check,industry_check,education_check;
+    public EditText email, phone, web;
+    private CheckBox services_check, fun_check, industry_check, education_check;
     Button save;
-  //  private Firebase mRootRef;
-   // private DatabaseReference mdb;
+    //  private Firebase mRootRef;
+    // private DatabaseReference mdb;
 
     FirebaseDatabase database;
     DatabaseReference ref;
@@ -63,21 +67,44 @@ public class Input extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
         //Toast.makeText(Input.this,"uspesno",Toast.LENGTH_LONG).show();
-        name_company = (EditText) findViewById(R.id.name_company);
-        name_company.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                name_company.getText().clear();
-            }
-        });
 
+        FirebaseApp.initializeApp(this);
+        name_company = (EditText) findViewById(R.id.name_company);
         address_company = (EditText) findViewById(R.id.address_company);
-        address_company.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                address_company.getText().clear();
-            }
-        });
+        latitude = (EditText) findViewById(R.id.latitude);
+        longitude = (EditText) findViewById(R.id.longitude);
+        email = (EditText) findViewById(R.id.email);
+        phone = (EditText) findViewById(R.id.phone_company);
+        web = (EditText) findViewById(R.id.web_company);
+        save = (Button) findViewById(R.id.btnInsert);
+        input = (ImageView) findViewById(R.id.input);
+        services_check = findViewById(R.id.services_check);
+        fun_check = findViewById(R.id.fun_check);
+        industry_check = findViewById(R.id.industry_check);
+        education_check = findViewById(R.id.education_check);
+    }
+
+    public void onClick(View v) {
+
+        String name = (String) name_company.getText().toString();
+        String address = (String) address_company.getText().toString();
+        String lat = (String) latitude.getText().toString();
+        String lon = (String) longitude.getText().toString();
+        String mail = (String) email.getText().toString();
+        String tel = (String) phone.getText().toString();
+        String web_site = (String) web.getText().toString();
+        boolean services = (boolean) services_check.isChecked();
+        boolean fun = (boolean) fun_check.isChecked();
+        boolean industry = (boolean) industry_check.isChecked();
+        boolean education = (boolean) education_check.isChecked();
+
+        String company_id;
+        ref=FirebaseDatabase.getInstance().getReference("Companies");
+        company_id = ref.push().getKey();
+        Company = new Companies(company_id,name,address,lat,lon,mail,tel,web_site,services,fun,industry,education);
+        ref.child(company_id).setValue(Company);
+
+    }
 
 //        input = (ImageView) findViewById(R.id.input);
 //        input.setOnClickListener(new View.OnClickListener() {
@@ -89,82 +116,18 @@ public class Input extends AppCompatActivity {
 //            }
 //        });
 
+    //mRootRef = new Firebase("https://companies-fb229.firebaseio.com/Company");
 
-        latitude = (EditText) findViewById(R.id.latitude);
-        latitude.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                latitude.getText().clear();
-            }
-        });
+    //mdb= FirebaseDatabase.getInstance().getReference();
+//    ----------------------------
 
-        longitude = (EditText) findViewById(R.id.longitude);
-        longitude.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                longitude.getText().clear();
-            }
-        });
-
-        email = (EditText) findViewById(R.id.email);
-        email.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                email.getText().clear();
-            }
-        });
-
-        phone = (EditText) findViewById(R.id.phone_company);
-        phone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                phone.getText().clear();
-            }
-        });
-
-        web = (EditText) findViewById(R.id.web_company);
-        web.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                web.getText().clear();
-            }
-        });
-
-        //mRootRef = new Firebase("https://companies-fb229.firebaseio.com/Company");
-        save = (Button) findViewById(R.id.btnInsert);
-
-
-
-
-        input = (ImageView) findViewById(R.id.input);
-        input.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                openIntent();
-
-            }
-        });
-        //mdb= FirebaseDatabase.getInstance().getReference();
-
-        services_check=findViewById(R.id.services_check);
-        fun_check=findViewById(R.id.fun_check);
-        industry_check=findViewById(R.id.industry_check);
-        education_check=findViewById(R.id.education_check);
-
-        services_check.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(services_check.isChecked()){
-
-                }
-            }
-        });
-
-
-        database = FirebaseDatabase.getInstance();
-        Company=new Companies();
-        ref = database.getReference("Companies").child("Company");
+//    database =FirebaseDatabase.getInstance();
+//    Company =new Companies();
+//
+//    ref =database.getReference("Companies").
+//
+//    child("Company");
+//    ----------------------------
 //        save.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -184,7 +147,7 @@ public class Input extends AppCompatActivity {
 //            }
 //        });
 
-       // Company = new Companies();
+    // Company = new Companies();
 //        save.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -193,32 +156,32 @@ public class Input extends AppCompatActivity {
 //            }
 //        });
 
-    }
-    private void getValues(){
-        Company.setName(name_company.getText().toString());
-        Company.setAddress(address_company.getText().toString());
-        Company.setLatitude(latitude.getText().toString());
-        Company.setLongitude(longitude.getText().toString());
-        Company.setEmail(email.getText().toString());
-        StringBuilder result=new StringBuilder();
-        if(services_check.isChecked()){
-            result.append("services");
-        }
-        if(fun_check.isChecked()){
-            result.append("fun");
-        }
-        if(industry_check.isChecked()){
-            result.append("industry");
-        }
-        if(education_check.isChecked()){
-            result.append("education");
-        }
-        Company.setCategory(result.toString());
-        Company.setTelephone(phone.getText().toString());
-        Company.setWeb_site(web.getText().toString());
+//    private void getValues() {
+//        Company.setName(name_company.getText().toString());
+//        Company.setAddress(address_company.getText().toString());
+//        Company.setLatitude(latitude.getText().toString());
+//        Company.setLongitude(longitude.getText().toString());
+//        Company.setEmail(email.getText().toString());
+//        StringBuilder result = new StringBuilder();
+//        if (services_check.isChecked()) {
+//            result.append("services");
+//        }
+//        if (fun_check.isChecked()) {
+//            result.append("fun");
+//        }
+//        if (industry_check.isChecked()) {
+//            result.append("industry");
+//        }
+//        if (education_check.isChecked()) {
+//            result.append("education");
+//        }
+//        Company.setCategory(result.toString());
+//        Company.setTelephone(phone.getText().toString());
+//        Company.setWeb_site(web.getText().toString());
+//
+//    }
 
-    }
-    public void openIntent(){
+    public void openIntent() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
@@ -230,24 +193,24 @@ public class Input extends AppCompatActivity {
 //
 //        }
 //    });
-
-    public void btnInsert (View view){
-
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-               getValues();
-               ref.setValue(Company);
-                Toast.makeText(Input.this,"Successfully inserted",Toast.LENGTH_LONG).show();
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
+//
+//    public void btnInsert(View view) {
+//
+//        ref.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                getValues();
+//                ref.setValue(Company);
+//                Toast.makeText(Input.this, "Successfully inserted", Toast.LENGTH_LONG).show();
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 //    public void addCompany (){
 //        String name = name_company.getText().toString();
 //        String address = address_company.getText().toString();
@@ -272,5 +235,5 @@ public class Input extends AppCompatActivity {
 //        else{
 //            Toast.makeText(Input.this,"Please fill the empty field",Toast.LENGTH_LONG).show();
 //        }
-//    }
+//    }}
 }
