c50f3ae09783974efc8c9d56a78d7fca2cec7f86
ivanaaaa <35149883+ivanaaaa@users.noreply.github.com>
2019-08-24 14:18:14 +0200

    education and fun fragments done

--- app/src/main/java/com/example/companies/FragmentEducation.java
+++ app/src/main/java/com/example/companies/FragmentEducation.java
@@ -1,6 +1,7 @@
 package com.example.companies;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,37 +10,79 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentEducation extends Fragment {
-    public FragmentEducation() {
-    }
+
 
     View v;
     private RecyclerView myrecyclerview;
-    private List<Services> lstEducation;
+    private List<Companies> EducationCompanies;
+    private FragmentList fragmentList;
 
+    public FragmentEducation() {
+    }
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v=inflater.inflate(R.layout.education_fragment,container,false);
+        v = inflater.inflate(R.layout.education_fragment, container, false);
         myrecyclerview = (RecyclerView) v.findViewById(R.id.education_recyclerview);
-        RecyclerViewAdapter recyclerAdapter= new RecyclerViewAdapter(getContext(),lstEducation);
+//        RecyclerViewAdapter recyclerAdapter= new RecyclerViewAdapter(getContext(),lstEducation);
         myrecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
-        myrecyclerview.setAdapter(recyclerAdapter);
+//        myrecyclerview.setAdapter(recyclerAdapter);
+
+        EducationCompanies = new ArrayList<>();
+        getCompaniesList();
         return v;
     }
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        lstEducation=new ArrayList<>();
-        lstEducation.add(new Services("University Goce Delcev","Stip","078965426","https//ugd.com",R.drawable.img1));
-        lstEducation.add(new Services("OU Kiril i Metodij","Kocani","078965427","https//kim.com",R.drawable.img1));
-        lstEducation.add(new Services("Univerzitet Kiril i Metodij","Skopje","078565426","https//ukim.com",R.drawable.img1));
-        lstEducation.add(new Services("SOU ljupco Santov","Kocani","078965487","https//souljp.com",R.drawable.img1));
-        lstEducation.add(new Services("SOU Kole Nihtenin","Stip","077965426","https//soukn.com",R.drawable.img1));
-        lstEducation.add(new Services("OU Vanco Prke","Stip","075123987","https//ouvp.com",R.drawable.img1));
-        lstEducation.add(new Services("Jane Sandanski","Stip","078965426","https//janesandanski.com",R.drawable.img1));
-        lstEducation.add(new Services("OU Nikola Karev","Kocani","078965957","https//ounk.com",R.drawable.img1));
+
+    private void getCompaniesList() {
+        final DatabaseReference database_reference = FirebaseDatabase.getInstance().getReference("Companies");
+        database_reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                EducationCompanies.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Companies company;
+                    company = snapshot.getValue(Companies.class);
+
+                    if (company.getCheck_education() == true) {
+                        EducationCompanies.add(company);
+                    }
+
+                }
+
+                fragmentList = new FragmentList(getContext(), EducationCompanies);
+                myrecyclerview.setAdapter(fragmentList);
+                fragmentList.OnItemClickListener((FragmentList.OnItemClickListener) FragmentEducation.this);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
+
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        lstEducation=new ArrayList<>();
+//        lstEducation.add(new Services("University Goce Delcev","Stip","078965426","https//ugd.com",R.drawable.img1));
+//        lstEducation.add(new Services("OU Kiril i Metodij","Kocani","078965427","https//kim.com",R.drawable.img1));
+//        lstEducation.add(new Services("Univerzitet Kiril i Metodij","Skopje","078565426","https//ukim.com",R.drawable.img1));
+//        lstEducation.add(new Services("SOU ljupco Santov","Kocani","078965487","https//souljp.com",R.drawable.img1));
+//        lstEducation.add(new Services("SOU Kole Nihtenin","Stip","077965426","https//soukn.com",R.drawable.img1));
+//        lstEducation.add(new Services("OU Vanco Prke","Stip","075123987","https//ouvp.com",R.drawable.img1));
+//        lstEducation.add(new Services("Jane Sandanski","Stip","078965426","https//janesandanski.com",R.drawable.img1));
+//        lstEducation.add(new Services("OU Nikola Karev","Kocani","078965957","https//ounk.com",R.drawable.img1));
+//    }
 }
--- app/src/main/java/com/example/companies/FragmentFun.java
+++ app/src/main/java/com/example/companies/FragmentFun.java
@@ -10,13 +10,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class FragmentFun extends Fragment  {
+public class FragmentFun extends Fragment {
     View v;
     private RecyclerView myrecyclerview;
-    private List<Services> lstFun;
+    private List<Companies> FunCompanies;
+    private FragmentList fragmentList;
 
     public FragmentFun() {
     }
@@ -24,25 +31,58 @@ public class FragmentFun extends Fragment  {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v=inflater.inflate(R.layout.fun_fragment,container,false);
+        v = inflater.inflate(R.layout.fun_fragment, container, false);
         myrecyclerview = (RecyclerView) v.findViewById(R.id.fun_recyclerview);
-        RecyclerViewAdapter recyclerAdapter= new RecyclerViewAdapter(getContext(),lstFun);
+//        RecyclerViewAdapter recyclerAdapter= new RecyclerViewAdapter(getContext(),lstFun);
         myrecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
-        myrecyclerview.setAdapter(recyclerAdapter);
+//        myrecyclerview.setAdapter(recyclerAdapter);
+
+        FunCompanies = new ArrayList<>();
+        getCompaniesList();
         return v;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        lstFun=new ArrayList<>();
-        lstFun.add(new Services("Igroteka","ulica1","078965426","https//Food and Drinks.com",R.drawable.img1));
-        lstFun.add(new Services("Club","ulica2","078965427","https//Club.com",R.drawable.img1));
-        lstFun.add(new Services("Car repair","ulica3","078565426","https//CarRepair.com",R.drawable.img1));
-        lstFun.add(new Services("Buystuff","ulica4","078965487","https//BuyStuff.com",R.drawable.img1));
-        lstFun.add(new Services("Mebel","ulica5","077965426","https//Mebel.com",R.drawable.img1));
-        lstFun.add(new Services("Osvetluvanje","ulica6","075123987","https//Osvetluvanje.com",R.drawable.img1));
-        lstFun.add(new Services("BuyCar","ulica7","078965426","https//Buycar.com",R.drawable.img1));
-        lstFun.add(new Services("Findsmth","ulica8","078965957","https//Findsmth.com",R.drawable.img1));
+    private void getCompaniesList() {
+        final DatabaseReference database_reference = FirebaseDatabase.getInstance().getReference("Companies");
+        database_reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                FunCompanies.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Companies company;
+                    company = snapshot.getValue(Companies.class);
+
+                    if (company.getCheck_fun() == true) {
+                        FunCompanies.add(company);
+                    }
+
+                }
+
+                fragmentList = new FragmentList(getContext(), FunCompanies);
+                myrecyclerview.setAdapter(fragmentList);
+                fragmentList.OnItemClickListener((FragmentList.OnItemClickListener) FragmentFun.this);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
+
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        lstFun=new ArrayList<>();
+//        lstFun.add(new Services("Igroteka","ulica1","078965426","https//Food and Drinks.com",R.drawable.img1));
+//        lstFun.add(new Services("Club","ulica2","078965427","https//Club.com",R.drawable.img1));
+//        lstFun.add(new Services("Car repair","ulica3","078565426","https//CarRepair.com",R.drawable.img1));
+//        lstFun.add(new Services("Buystuff","ulica4","078965487","https//BuyStuff.com",R.drawable.img1));
+//        lstFun.add(new Services("Mebel","ulica5","077965426","https//Mebel.com",R.drawable.img1));
+//        lstFun.add(new Services("Osvetluvanje","ulica6","075123987","https//Osvetluvanje.com",R.drawable.img1));
+//        lstFun.add(new Services("BuyCar","ulica7","078965426","https//Buycar.com",R.drawable.img1));
+//        lstFun.add(new Services("Findsmth","ulica8","078965957","https//Findsmth.com",R.drawable.img1));
+//    }
 }
